18a1c4c61b3356dded7a050fa545eb037b490af8
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2025-08-10 08:04:02 -0400

    Created recursive fib solution

--- fibSequence.js
+++ fibSequence.js
@@ -14,8 +14,6 @@ function fibs(fibLength) {
         case 2: 
             return [0,1];
             break;
-        
-
     };
     const fibSequence = [0,1]
     for (let i = 2; i < fibLength; i++) {
@@ -24,4 +22,27 @@ function fibs(fibLength) {
     };
     return fibSequence;
 }
-console.log(fibs(8))
+//console.log(fibs(8))
+
+function fibsRec(fibLength) {
+    switch (fibLength) {
+        case 0: 
+            return [];
+            break;
+        case 1: 
+            return [0];
+            break;
+        case 2: 
+            return [0,1];
+            break;
+    };
+    let fibArray = fibsRec(fibLength - 1);
+    fibArray.push((fibArray[fibArray.length-1] + fibArray[fibArray.length-2]));
+    return fibArray //fibArray.push((fibArray[fibArray.length-1] + fibArray[fibArray.length-2]));
+}
+console.log(fibsRec(8))
+
+
+
+
+
